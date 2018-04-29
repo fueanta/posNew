1c437f81bd38b9662b8fa17c3d932967807db10a
Saif Hassan <saiftheboss7@gmail.com>
2018-04-30 03:43:53 +0600

    Supplier and Create Supplier UI

---
+++ Forms/Supplier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms
+{
+    public partial class Supplier : MetroFramework.Forms.MetroForm
+    {
+        public Supplier()
+        {
+            InitializeComponent();
+        }
+
+        private void createBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
