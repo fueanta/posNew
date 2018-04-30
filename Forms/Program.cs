cc21beed87e7e8206cef504576e910af305f7987
Saif Hassan <saiftheboss7@gmail.com>
2018-04-30 19:35:41 +0600

    High DPI Support

--- Forms/Program.cs
+++ Forms/Program.cs
@@ -14,9 +14,16 @@ namespace Forms
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major == 6)
+                SetProcessDPIAware();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Customer_TypeForm());
         }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+        //For C# High Resolution
     }
 }
