a095ca5eb69916f2141023dd79434d8bc75ebb90
fueanta <fueanta@gmail.com>
2018-05-01 13:13:24 +0600

    kam kor

---
+++ Forms/HomePage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms
+{
+    public partial class HomePage : MetroFramework.Forms.MetroForm
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void tileEmployeeManagerHRD_Click(object sender, EventArgs e)
+        {
+            CustomersViews customer = new CustomersViews();
+            customer.Show();
+            this.Hide();
+            
+        }
+    }
+}
