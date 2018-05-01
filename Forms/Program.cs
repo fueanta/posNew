01418e4bb7f9ba721b0069d3fe2ed572ee4317c5
Saif Hassan <saiftheboss7@gmail.com>
2018-05-01 13:00:32 +0600

    saif home

--- Forms/Program.cs
+++ Forms/Program.cs
@@ -19,7 +19,7 @@ namespace Forms
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new EmployeeViews());
+            Application.Run(new HomePage());
         }
 
         [System.Runtime.InteropServices.DllImport("user32.dll")]
