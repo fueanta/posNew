d9614af62be16689ca76431f11a17e69a2c29db8
Saif Hassan <saiftheboss7@gmail.com>
2018-05-01 20:23:16 +0600

    Added all Models

---
+++ Software/Model/POS_Purchase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software.Model
+{
+    public class POS_Purchase
+    {
+        public int Id { get; set; }        
+        public string Description { get; set; }
+        public DateTime Time { get; set; }
+        public int Employee_Id { get; set; }
+        public int Supplier_Id { get; set; }
+        public int Cost { get; set; }
+        public int Discount_Rate { get; set; }
+        public int Total_Cost { get; set; }
+        
+
+    }
+}
---
+++ Software/Model/Purchase_Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software.Model
+{
+    public class Purchase_Ingredient
+    {
+        public int Purchase_ID { get; set; }
+        public int Ingredient_ID { get; set; }
+        public int Amount { get; set; }
+    }
+}
